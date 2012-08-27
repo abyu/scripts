09aa8bece892ca512419a9848a3b03af93b8bcac
Kishore <abyu.kishore@thoughtworks.com>
2012-08-27 12:09:13 +0530

    Removing few echo statements and added parameter count check

--- buildVersion.sh
+++ buildVersion.sh
@@ -1,31 +1,37 @@
+#!/bin/bash
+
 IFS=$IFS
+latestVersion=$(git describe $branch --abbrev=0)
+if [ $# -lt 1 ]; then
+	echo "Available build version: $latestVersion"
+	exit 0
+fi
 echo "Searching for commit(s) with messages containing text $1" 
 branch=$2
 commitIdsRaw=$(git log $branch --grep=$1 --pretty="%h"| tr '\n' ';')
 IFS=';'
 commitIds=($commitIdsRaw)
 IFS=$OIFS
-commitsCount = ${#commitIds[@]}
-if [ commitsCount -gt "0" ] then
-	echo "Commit(s) with id ${commitIds[@]} found" 
+commitsCount=${#commitIds[@]}
+if [ "$commitsCount" -gt "0" ]; then
+	#echo "Commit(s) with id ${commitIds[@]} found" 
 	for commitId in ${commitIds[@]}
 	do
-		commitMessage=$(git log $branch $commitId...$commitId^ --pretty="%s")
+		commitMessage=$(git log $commitId...$commitId^ --pretty="%s")
 		echo "Looking for build version containing commit <$commitMessage> "
-		buildVersionsRaw=$(git log $branch HEAD...$commitId^ --pretty="%h%d" | sed -n s/'.*\(([0-9\., ]*)\).*'/'\1'/gp | tr '\n' ';')
+		buildVersionsRaw=$(git log HEAD...$commitId^ --pretty="%h%d" | sed -n s/'.*\(([0-9\., ]*)\).*'/'\1'/gp | tr '\n' ';')
 		IFS=';'
 		buildVersions=($buildVersionsRaw)
 		IFS=OIFS
 		versionsCount=${#buildVersions[@]}
 	if [ "$versionsCount" -lt "1" ]; then
-		echo "They build is not available yet. Make sure your repo is updated and try again\n"
+		echo "They build is not available yet. Make sure your repo is updated and try again"
 	else
-		echo "The commit is present in the build version - ${buildVersions[$versionsCount-1]}\n"
+		echo "The commit is present in the build version - ${buildVersions[$versionsCount-1]}"
 	fi
 	done
 else
 	echo "No commits found"
 fi
-latestVersion=$(git describe $branch --abbrev=0)
 echo "Available Latest build version is $latestVersion"
 echo "Enjoy! Have a nice day !"
